e71c999b08bcf798300455f87186bd68ca946682
henry199101 <331243543@qq.com>
2017-12-08 14:38:48 +0800

    add converters.py

--- chapter30_class_design/converters.py
+++ chapter30_class_design/converters.py
@@ -3,8 +3,7 @@ from streams import Processor
 class Uppercase(Processor):
 	def converter(self, data):
 		return data.upper()
-
-if __name__ == '__main__':
-	import sys
-	obj = Uppercase(open('spam.txt'), sys.stdout)
-	obj.process()
+	if __name__ == '__main__':
+		import sys
+		obj = Uppercase(open('spam.txt'), sys.stdout)
+		obj.process()
