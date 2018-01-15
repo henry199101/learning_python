c147ccec3ce37e0685ffa59b89e3f9cb55e6a8b5
henry199101 <331243543@qq.com>
2018-01-16 00:27:53 +0800

    add many things

--- chapter16_function_base/intersectDemo.py
+++ chapter16_function_base/intersectDemo.py
@@ -1,5 +1,3 @@
-# intersectDemo.py
-
 def intersect(seq1, seq2):
 	res = []
 	for x in seq1:
@@ -9,4 +7,8 @@ def intersect(seq1, seq2):
 
 s1 = "SPAM"
 s2 = "SCAM"
-print intersect(s1, s2)
+print(intersect(s1, s2))
+
+print(intersect([1, 2, 3], (1, 4)))
+
+[x for x in s1 if x in s2]
