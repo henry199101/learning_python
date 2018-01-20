a50eb628ee6afad498eb92c7a540a1a0d58c97f1
henry199101 <331243543@qq.com>
2018-01-20 17:24:05 +0800

    add many

---
+++ chapter18_parameters_arguments/temp.py
@@ -0,0 +1,9 @@
+import sys
+
+def print30(*args, sep=' ', end='\n', file=sys.stdout):
+	output = ''
+	first = True
+	for arg in args:
+		output += ('' if first else sep) + str(arg)
+		first = False
+	file.write(output + end)
