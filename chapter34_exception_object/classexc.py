e4d2948e118f915949c3a9d1b5939a8ba959c9d3
henry199101 <331243543@qq.com>
2018-02-06 14:54:00 +0800

    add many

--- chapter34_exception_object/classexc.py
+++ chapter34_exception_object/classexc.py
@@ -2,19 +2,19 @@ class General(Exception): pass
 class Specific1(General): pass
 class Specific2(General): pass
 
-def raise0():
+def raiser0():
 	X = General()
 	raise X
 
-def raise1():
+def raiser1():
 	X = Specific1()
 	raise X
 
-def raise2():
+def raiser2():
 	X = Specific2()
 	raise X
 
-for func in (raise0, raise1, raise2):
+for func in (raiser0, raiser1, raiser2):
 	try:
 		func()
 	except General:
